ddf6238221964e5ab7e9c48b322cae0d9e533ac0
Andrii Sokolenko <a.sokolenko@samsung.com>
2015-12-09 17:35:55 +0200

    First draft
    Change-Id: Ic0835f7a939c1bc13dd0dc62772b8382989c4dbc
    Signed-off-by: Andrii Sokolenko <a.sokolenko@samsung.com>

---
+++ src/tdm_sprd.h
@@ -0,0 +1,130 @@
+#ifndef _TDM_SPRD_H_
+#define _TDM_SPRD_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+#include <xf86drm.h>
+#include <xf86drmMode.h>
+#include <tbm_surface.h>
+#include <tbm_surface_internal.h>
+#include <tdm_backend.h>
+#include <tdm_log.h>
+#include <tdm_list.h>
+
+/* sprd backend functions (display) */
+tdm_error    sprd_display_get_capabilitiy(tdm_backend_data *bdata, tdm_caps_display *caps);
+tdm_error    sprd_display_get_pp_capability(tdm_backend_data *bdata, tdm_caps_pp *caps);
+tdm_output** sprd_display_get_outputs(tdm_backend_data *bdata, int *count, tdm_error *error);
+tdm_error    sprd_display_get_fd(tdm_backend_data *bdata, int *fd);
+tdm_error    sprd_display_handle_events(tdm_backend_data *bdata);
+tdm_pp*      sprd_display_create_pp(tdm_backend_data *bdata, tdm_error *error);
+tdm_error    sprd_output_get_capability(tdm_output *output, tdm_caps_output *caps);
+tdm_layer**  sprd_output_get_layers(tdm_output *output, int *count, tdm_error *error);
+tdm_error    sprd_output_set_property(tdm_output *output, unsigned int id, tdm_value value);
+tdm_error    sprd_output_get_property(tdm_output *output, unsigned int id, tdm_value *value);
+tdm_error    sprd_output_wait_vblank(tdm_output *output, int interval, int sync, void *user_data);
+tdm_error    sprd_output_set_vblank_handler(tdm_output *output, tdm_output_vblank_handler func);
+tdm_error    sprd_output_commit(tdm_output *output, int sync, void *user_data);
+tdm_error    sprd_output_set_commit_handler(tdm_output *output, tdm_output_commit_handler func);
+tdm_error    sprd_output_set_dpms(tdm_output *output, tdm_output_dpms dpms_value);
+tdm_error    sprd_output_get_dpms(tdm_output *output, tdm_output_dpms *dpms_value);
+tdm_error    sprd_output_set_mode(tdm_output *output, tdm_output_mode *mode);
+tdm_error    sprd_output_get_mode(tdm_output *output, const tdm_output_mode **mode);
+tdm_error    sprd_layer_get_capability(tdm_layer *layer, tdm_caps_layer *caps);
+tdm_error    sprd_layer_set_property(tdm_layer *layer, unsigned int id, tdm_value value);
+tdm_error    sprd_layer_get_property(tdm_layer *layer, unsigned int id, tdm_value *value);
+tdm_error    sprd_layer_set_info(tdm_layer *layer, tdm_info_layer *info);
+tdm_error    sprd_layer_get_info(tdm_layer *layer, tdm_info_layer *info);
+tdm_error    sprd_layer_set_buffer(tdm_layer *layer, tbm_surface_h buffer);
+tdm_error    sprd_layer_unset_buffer(tdm_layer *layer);
+void         sprd_pp_destroy(tdm_pp *pp);
+tdm_error    sprd_pp_set_info(tdm_pp *pp, tdm_info_pp *info);
+tdm_error    sprd_pp_attach(tdm_pp *pp, tbm_surface_h src, tbm_surface_h dst);
+tdm_error    sprd_pp_commit(tdm_pp *pp);
+tdm_error    sprd_pp_set_done_handler(tdm_pp *pp, tdm_pp_done_handler func, void *user_data);
+
+
+/* sprd module internal macros, structures, functions */
+#define NEVER_GET_HERE() TDM_ERR("** NEVER GET HERE **")
+
+#define C(b,m)              (((b) >> (m)) & 0xFF)
+#define B(c,s)              ((((unsigned int)(c)) & 0xff) << (s))
+#define FOURCC(a,b,c,d)     (B(d,24) | B(c,16) | B(b,8) | B(a,0))
+#define FOURCC_STR(id)      C(id,0), C(id,8), C(id,16), C(id,24)
+
+#define IS_RGB(format)      (format == TBM_FORMAT_XRGB8888 || format == TBM_FORMAT_ARGB8888 || \
+                             format == TBM_FORMAT_XBGR8888 || format == TBM_FORMAT_ABGR8888)
+
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
+#define MAX(a,b) (((a) > (b)) ? (a) : (b))
+#define MIN(a,b) (((a) < (b)) ? (a) : (b))
+#define SWAP(a, b)  ({int t; t = a; a = b; b = t;})
+#define ROUNDUP(x)  (ceil (floor ((float)(height) / 4)))
+
+#define ALIGN_TO_16B(x)    ((((x) + (1 <<  4) - 1) >>  4) <<  4)
+#define ALIGN_TO_32B(x)    ((((x) + (1 <<  5) - 1) >>  5) <<  5)
+#define ALIGN_TO_128B(x)   ((((x) + (1 <<  7) - 1) >>  7) <<  7)
+#define ALIGN_TO_2KB(x)    ((((x) + (1 << 11) - 1) >> 11) << 11)
+#define ALIGN_TO_8KB(x)    ((((x) + (1 << 13) - 1) >> 13) << 13)
+#define ALIGN_TO_64KB(x)   ((((x) + (1 << 16) - 1) >> 16) << 16)
+
+#define RETURN_VAL_IF_FAIL(cond, val) {\
+    if (!(cond)) {\
+        TDM_ERR("'%s' failed", #cond);\
+        return val;\
+    }\
+}
+
+typedef struct _tdm_sprd_data
+{
+    tdm_display *dpy;
+    int drm_fd;
+    int fb_fd;
+    void * sprd_drm_dev;
+
+    /* If true, it means that the device has many planes for one crtc. If false,
+     * planes are dedicated to specific crtc.
+     */
+    int has_zpos_info;
+
+    /* If has_zpos_info is false and is_immutable_zpos is true, it means that
+     * planes are dedicated to specific crtc.
+     */
+    int is_immutable_zpos;
+
+    drmModeResPtr mode_res;
+    drmModePlaneResPtr plane_res;
+
+    struct list_head output_list;
+    struct list_head buffer_list;
+} tdm_sprd_data;
+
+uint32_t     tdm_sprd_format_to_drm_format(tbm_format format);
+tbm_format   tdm_sprd_format_to_tbm_format(uint32_t format);
+
+tdm_error    tdm_sprd_display_create_output_list(tdm_sprd_data *sprd_data);
+void         tdm_sprd_display_destroy_output_list(tdm_sprd_data *sprd_data);
+tdm_error    tdm_sprd_display_create_layer_list(tdm_sprd_data *sprd_data);
+tdm_error    tdm_sprd_display_set_property(tdm_sprd_data *sprd_data,
+                                             unsigned int obj_id, unsigned int obj_type,
+                                             const char *name, unsigned int value);
+tdm_error    tdm_sprd_display_get_property(tdm_sprd_data *sprd_data,
+                                             unsigned int obj_id, unsigned int obj_type,
+                                             const char *name, unsigned int *value, int *is_immutable);
+
+tdm_error    tdm_sprd_pp_get_capability(tdm_sprd_data *sprd_data, tdm_caps_pp *caps);
+tdm_pp*      tdm_sprd_pp_create(tdm_sprd_data *sprd_data, tdm_error *error);
+void         tdm_sprd_pp_handler(unsigned int prop_id, unsigned int *buf_idx,
+                                   unsigned int tv_sec, unsigned int tv_usec, void *data);
+
+#endif /* _TDM_SPRD_H_ */
---
+++ src/tdm_sprd_pp.c
@@ -0,0 +1,475 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "tdm_sprd.h"
+
+typedef struct _tdm_sprd_pp_buffer
+{
+    int index;
+    tbm_surface_h src;
+    tbm_surface_h dst;
+
+    struct list_head link;
+} tdm_sprd_pp_buffer;
+
+typedef struct _tdm_sprd_pp_data
+{
+    tdm_sprd_data *sprd_data;
+
+    unsigned int prop_id;
+
+    tdm_info_pp info;
+    int info_changed;
+
+    struct list_head pending_buffer_list;
+    struct list_head buffer_list;
+
+    tdm_pp_done_handler done_func;
+    void *done_user_data;
+
+    int startd;
+    int first_event;
+
+    struct list_head link;
+} tdm_sprd_pp_data;
+
+#if 0
+static tbm_format pp_formats[] =
+{
+    TBM_FORMAT_ARGB8888,
+    TBM_FORMAT_XRGB8888,
+    TBM_FORMAT_YUYV,
+    TBM_FORMAT_UYVY,
+    TBM_FORMAT_NV12,
+    TBM_FORMAT_NV21,
+    TBM_FORMAT_YUV420,
+    TBM_FORMAT_YVU420
+};
+#else
+static tbm_format *pp_formats = NULL;
+#endif
+
+#if 0
+#define NUM_PP_FORMAT   (sizeof(pp_formats) / sizeof(pp_formats[0]))
+#else
+#define NUM_PP_FORMAT 0
+#endif
+
+static int pp_list_init;
+static struct list_head pp_list;
+
+static int
+_get_index(tdm_sprd_pp_data *pp_data)
+{
+    tdm_sprd_pp_buffer *buffer;
+    int ret = 0;
+
+    while (1)
+    {
+        int found = 0;
+        LIST_FOR_EACH_ENTRY(buffer, &pp_data->pending_buffer_list, link)
+        {
+            if (ret == buffer->index)
+            {
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+            LIST_FOR_EACH_ENTRY(buffer, &pp_data->buffer_list, link)
+            {
+                if (ret == buffer->index)
+                {
+                    found = 1;
+                    break;
+                }
+            }
+        if (!found)
+            break;
+        ret++;
+    }
+
+    return ret;
+}
+#if 0
+static tdm_error
+_tdm_sprd_pp_set(tdm_sprd_pp_data *pp_data)
+{
+    tdm_sprd_data *sprd_data = pp_data->sprd_data;
+    tdm_info_pp *info = &pp_data->info;
+    struct drm_sprd_ipp_property property;
+    int ret = 0;
+
+    CLEAR(property);
+    property.config[0].ops_id = EXYNOS_DRM_OPS_SRC;
+    property.config[0].fmt = tdm_sprd_format_to_drm_format(info->src_config.format);
+    memcpy(&property.config[0].sz, &info->src_config.size, sizeof(tdm_size));
+    memcpy(&property.config[0].pos, &info->src_config.pos, sizeof(tdm_pos));
+    property.config[1].ops_id = EXYNOS_DRM_OPS_DST;
+    property.config[1].degree = info->transform / 4;
+    property.config[1].flip = (info->transform > 3) ? EXYNOS_DRM_FLIP_HORIZONTAL : 0;
+    property.config[1].fmt = tdm_sprd_format_to_drm_format(info->dst_config.format);
+    memcpy(&property.config[1].sz, &info->dst_config.size, sizeof(tdm_size));
+    memcpy(&property.config[1].pos, &info->dst_config.pos, sizeof(tdm_pos));
+    property.cmd = IPP_CMD_M2M;
+    property.prop_id = pp_data->prop_id;
+
+    TDM_DBG("src : flip(%x) deg(%d) fmt(%c%c%c%c) sz(%dx%d) pos(%d,%d %dx%d)  ",
+            property.config[0].flip, property.config[0].degree, FOURCC_STR(property.config[0].fmt),
+            property.config[0].sz.hsize, property.config[0].sz.vsize,
+            property.config[0].pos.x, property.config[0].pos.y, property.config[0].pos.w, property.config[0].pos.h);
+    TDM_DBG("dst : flip(%x) deg(%d) fmt(%c%c%c%c) sz(%dx%d) pos(%d,%d %dx%d)  ",
+            property.config[1].flip, property.config[1].degree, FOURCC_STR(property.config[1].fmt),
+            property.config[1].sz.hsize, property.config[1].sz.vsize,
+            property.config[1].pos.x, property.config[1].pos.y, property.config[1].pos.w, property.config[1].pos.h);
+
+    ret = ioctl(sprd_data->drm_fd, DRM_IOCTL_EXYNOS_IPP_SET_PROPERTY, &property);
+    if (ret)
+    {
+        TDM_ERR("failed: %m");
+        return TDM_ERROR_OPERATION_FAILED;
+    }
+
+    TDM_DBG("success. prop_id(%d) ", property.prop_id);
+    pp_data->prop_id = property.prop_id;
+    return TDM_ERROR_NONE;
+}
+#endif
+#if 0
+static tdm_error
+_tdm_sprd_pp_queue(tdm_sprd_pp_data *pp_data, tdm_sprd_pp_buffer *buffer, enum drm_sprd_ipp_buf_type type)
+{
+    tdm_sprd_data *sprd_data = pp_data->sprd_data;
+    struct drm_sprd_ipp_queue_buf buf;
+    int i, bo_num, ret = 0;
+
+    CLEAR(buf);
+    buf.prop_id = pp_data->prop_id;
+    buf.ops_id = EXYNOS_DRM_OPS_SRC;
+    buf.buf_type = type;
+    buf.buf_id = buffer->index;
+    buf.user_data = (__u64)(uintptr_t)pp_data;
+    bo_num = tbm_surface_internal_get_num_bos(buffer->src);
+    for (i = 0; i < EXYNOS_DRM_PLANAR_MAX && i < bo_num; i++)
+    {
+        tbm_bo bo = tbm_surface_internal_get_bo(buffer->src, i);
+        buf.handle[i] = (__u32)tbm_bo_get_handle(bo, TBM_DEVICE_DEFAULT).u32;
+    }
+
+    TDM_DBG("prop_id(%d) ops_id(%d) ctrl(%d) id(%d) handles(%x %x %x). ",
+            buf.prop_id, buf.ops_id, buf.buf_type, buf.buf_id,
+            buf.handle[0], buf.handle[1], buf.handle[2]);
+
+    ret = ioctl(sprd_data->drm_fd, DRM_IOCTL_EXYNOS_IPP_QUEUE_BUF, &buf);
+    if (ret)
+    {
+        TDM_ERR("src failed. prop_id(%d) op(%d) buf(%d) id(%d). %m",
+                buf.prop_id, buf.ops_id, buf.buf_type, buf.buf_id);
+        return TDM_ERROR_OPERATION_FAILED;
+    }
+
+    CLEAR(buf);
+    buf.prop_id = pp_data->prop_id;
+    buf.ops_id = EXYNOS_DRM_OPS_DST;
+    buf.buf_type = type;
+    buf.buf_id = buffer->index;
+    buf.user_data = (__u64)(uintptr_t)pp_data;
+    bo_num = tbm_surface_internal_get_num_bos(buffer->dst);
+    for (i = 0; i < EXYNOS_DRM_PLANAR_MAX && i < bo_num; i++)
+    {
+        tbm_bo bo = tbm_surface_internal_get_bo(buffer->dst, i);
+        buf.handle[i] = (__u32)tbm_bo_get_handle(bo, TBM_DEVICE_DEFAULT).u32;
+    }
+
+    TDM_DBG("prop_id(%d) ops_id(%d) ctrl(%d) id(%d) handles(%x %x %x). ",
+            buf.prop_id, buf.ops_id, buf.buf_type, buf.buf_id,
+            buf.handle[0], buf.handle[1], buf.handle[2]);
+
+    ret = ioctl(sprd_data->drm_fd, DRM_IOCTL_EXYNOS_IPP_QUEUE_BUF, &buf);
+    if (ret)
+    {
+        TDM_ERR("dst failed. prop_id(%d) op(%d) buf(%d) id(%d). %m",
+                buf.prop_id, buf.ops_id, buf.buf_type, buf.buf_id);
+        return TDM_ERROR_OPERATION_FAILED;
+    }
+
+    TDM_DBG("success. prop_id(%d)", buf.prop_id);
+
+    return TDM_ERROR_NONE;
+}
+#endif
+#if 0
+static tdm_error
+_tdm_sprd_pp_cmd(tdm_sprd_pp_data *pp_data, enum drm_sprd_ipp_ctrl cmd)
+{
+    tdm_sprd_data *sprd_data = pp_data->sprd_data;
+    struct drm_sprd_ipp_cmd_ctrl ctrl;
+    int ret = 0;
+
+    ctrl.prop_id = pp_data->prop_id;
+    ctrl.ctrl = cmd;
+
+    TDM_DBG("prop_id(%d) ctrl(%d). ", ctrl.prop_id, ctrl.ctrl);
+
+    ret = ioctl(sprd_data->drm_fd, DRM_IOCTL_EXYNOS_IPP_CMD_CTRL, &ctrl);
+    if (ret)
+    {
+        TDM_ERR("failed. prop_id(%d) ctrl(%d). %m", ctrl.prop_id, ctrl.ctrl);
+        return TDM_ERROR_OPERATION_FAILED;
+    }
+
+    TDM_DBG("success. prop_id(%d) ", ctrl.prop_id);
+
+    return TDM_ERROR_NONE;
+}
+#endif
+void
+tdm_sprd_pp_handler(unsigned int prop_id, unsigned int *buf_idx,
+                      unsigned int tv_sec, unsigned int tv_usec, void *data)
+{
+    tdm_sprd_pp_data *found = NULL, *pp_data = data;
+    tdm_sprd_pp_buffer *b = NULL, *bb = NULL, *dequeued_buffer = NULL;
+
+    if (!pp_data || !buf_idx)
+    {
+        TDM_ERR("invalid params");
+        return;
+    }
+
+    LIST_FOR_EACH_ENTRY(found, &pp_list, link)
+    {
+        if (found == pp_data)
+            break;
+    }
+    if (!found)
+        return;
+
+    TDM_DBG("pp_data(%p) index(%d, %d)", pp_data, buf_idx[0], buf_idx[1]);
+
+    LIST_FOR_EACH_ENTRY_SAFE(b, bb, &pp_data->buffer_list, link)
+    {
+        if (buf_idx[0] == b->index)
+        {
+            dequeued_buffer = b;
+            LIST_DEL(&dequeued_buffer->link);
+            TDM_DBG("dequeued: %d", dequeued_buffer->index);
+            break;
+        }
+    }
+
+    if (!dequeued_buffer)
+    {
+        TDM_ERR("not found buffer index: %d", buf_idx[0]);
+        return;
+    }
+
+    if (!pp_data->first_event)
+    {
+        TDM_DBG("pp(%p) got a first event. ", pp_data);
+        pp_data->first_event = 1;
+    }
+
+    if (pp_data->done_func)
+        pp_data->done_func(pp_data,
+                           dequeued_buffer->src,
+                           dequeued_buffer->dst,
+                           pp_data->done_user_data);
+    free(dequeued_buffer);
+}
+
+tdm_error
+tdm_sprd_pp_get_capability(tdm_sprd_data *sprd_data, tdm_caps_pp *caps)
+{
+    int i;
+
+    if (!caps)
+    {
+        TDM_ERR("invalid params");
+        return TDM_ERROR_INVALID_PARAMETER;
+    }
+
+    caps->capabilities = TDM_PP_CAPABILITY_ASYNC;
+
+    caps->format_count = NUM_PP_FORMAT;
+    caps->formats = NULL;
+    if (NUM_PP_FORMAT)
+    {
+        /* will be freed in frontend */
+        caps->formats = calloc(1, sizeof pp_formats);
+        if (!caps->formats)
+        {
+            TDM_ERR("alloc failed");
+            return TDM_ERROR_OUT_OF_MEMORY;
+        }
+        for (i = 0; i < caps->format_count; i++)
+            caps->formats[i] = pp_formats[i];
+    }
+
+    caps->min_w = 16;
+    caps->min_h = 8;
+    caps->max_w = -1;   /* not defined */
+    caps->max_h = -1;
+    caps->preferred_align = 16;
+
+    return TDM_ERROR_NONE;
+}
+
+tdm_pp*
+tdm_sprd_pp_create(tdm_sprd_data *sprd_data, tdm_error *error)
+{
+    tdm_sprd_pp_data *pp_data = calloc(1, sizeof(tdm_sprd_pp_data));
+    if (!pp_data)
+    {
+        TDM_ERR("alloc failed");
+        if (error)
+            *error = TDM_ERROR_OUT_OF_MEMORY;
+        return NULL;
+    }
+
+    pp_data->sprd_data = sprd_data;
+
+    LIST_INITHEAD(&pp_data->pending_buffer_list);
+    LIST_INITHEAD(&pp_data->buffer_list);
+
+    if (!pp_list_init)
+    {
+        pp_list_init = 1;
+        LIST_INITHEAD(&pp_list);
+    }
+    LIST_ADDTAIL(&pp_data->link, &pp_list);
+
+    return pp_data;
+}
+
+void
+sprd_pp_destroy(tdm_pp *pp)
+{
+    tdm_sprd_pp_data *pp_data = pp;
+    tdm_sprd_pp_buffer *b = NULL, *bb = NULL;
+
+    if (!pp_data)
+        return;
+    LIST_FOR_EACH_ENTRY_SAFE(b, bb, &pp_data->pending_buffer_list, link)
+    {
+        LIST_DEL(&b->link);
+        free(b);
+    }
+
+    LIST_FOR_EACH_ENTRY_SAFE(b, bb, &pp_data->buffer_list, link)
+    {
+        LIST_DEL(&b->link);
+#if 0
+        _tdm_sprd_pp_queue(pp_data, b, IPP_BUF_DEQUEUE);
+#endif
+        free(b);
+    }
+#if 0
+    if (pp_data->prop_id)
+        _tdm_sprd_pp_cmd(pp_data, IPP_CTRL_STOP);
+#endif
+    LIST_DEL(&pp_data->link);
+
+    free(pp_data);
+}
+
+tdm_error
+sprd_pp_set_info(tdm_pp *pp, tdm_info_pp *info)
+{
+    tdm_sprd_pp_data *pp_data = pp;
+
+    RETURN_VAL_IF_FAIL(pp_data, TDM_ERROR_INVALID_PARAMETER);
+    RETURN_VAL_IF_FAIL(info, TDM_ERROR_INVALID_PARAMETER);
+
+    if (info->sync)
+    {
+        TDM_ERR("not support sync mode currently");
+        return TDM_ERROR_INVALID_PARAMETER;
+    }
+
+    pp_data->info = *info;
+    pp_data->info_changed = 1;
+
+    return TDM_ERROR_NONE;
+}
+
+tdm_error
+sprd_pp_attach(tdm_pp *pp, tbm_surface_h src, tbm_surface_h dst)
+{
+    tdm_sprd_pp_data *pp_data = pp;
+    tdm_sprd_pp_buffer *buffer;
+
+    RETURN_VAL_IF_FAIL(pp_data, TDM_ERROR_INVALID_PARAMETER);
+    RETURN_VAL_IF_FAIL(src, TDM_ERROR_INVALID_PARAMETER);
+    RETURN_VAL_IF_FAIL(dst, TDM_ERROR_INVALID_PARAMETER);
+
+    buffer = calloc(1, sizeof(tdm_sprd_pp_buffer));
+    if (!buffer)
+    {
+        TDM_ERR("alloc failed");
+        return TDM_ERROR_NONE;
+    }
+
+    LIST_ADDTAIL(&buffer->link, &pp_data->pending_buffer_list);
+    buffer->index = _get_index(pp_data);
+    buffer->src = src;
+    buffer->dst = dst;
+
+    return TDM_ERROR_NONE;
+}
+
+tdm_error
+sprd_pp_commit(tdm_pp *pp)
+{
+#if 0
+    tdm_sprd_pp_data *pp_data = pp;
+    tdm_sprd_pp_buffer *b = NULL, *bb = NULL;
+    tdm_error ret = TDM_ERROR_NONE;
+    RETURN_VAL_IF_FAIL(pp_data, TDM_ERROR_INVALID_PARAMETER);
+    if (pp_data->info_changed)
+    {
+        if (pp_data->startd)
+            _tdm_sprd_pp_cmd(pp_data, IPP_CTRL_PAUSE);
+
+        ret = _tdm_sprd_pp_set(pp_data);
+        if (ret < 0)
+            return TDM_ERROR_OPERATION_FAILED;
+    }
+
+    LIST_FOR_EACH_ENTRY_SAFE(b, bb, &pp_data->pending_buffer_list, link)
+    {
+        LIST_DEL(&b->link);
+        _tdm_sprd_pp_queue(pp_data, b, IPP_BUF_ENQUEUE);
+        TDM_DBG("queued: %d", b->index);
+        LIST_ADDTAIL(&b->link, &pp_data->buffer_list);
+    }
+
+    if (pp_data->info_changed)
+    {
+        pp_data->info_changed = 0;
+
+        if (!pp_data->startd)
+        {
+            pp_data->startd = 1;
+            _tdm_sprd_pp_cmd(pp_data, IPP_CTRL_PLAY);
+        }
+        else
+            _tdm_sprd_pp_cmd(pp_data, IPP_CTRL_RESUME);
+    }
+#endif
+    return TDM_ERROR_NONE;
+}
+
+tdm_error
+sprd_pp_set_done_handler(tdm_pp *pp, tdm_pp_done_handler func, void *user_data)
+{
+    tdm_sprd_pp_data *pp_data = pp;
+
+    RETURN_VAL_IF_FAIL(pp_data, TDM_ERROR_INVALID_PARAMETER);
+    RETURN_VAL_IF_FAIL(func, TDM_ERROR_INVALID_PARAMETER);
+
+    pp_data->done_func = func;
+    pp_data->done_user_data = user_data;
+
+    return TDM_ERROR_NONE;
+}

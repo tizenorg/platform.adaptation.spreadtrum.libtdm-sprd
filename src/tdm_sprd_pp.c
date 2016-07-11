17763c8212deac8dba4a93788d1363623fa43ba1
Andrii Sokolenko <a.sokolenko@samsung.com>
2016-07-11 14:32:30 +0300

    Fix IPP fail when frequently change converter property
    Change-Id: I23e3d8d6abcdc939964a7e161a64d56c1e7c1e82
    Signed-off-by: Andrii Sokolenko <a.sokolenko@samsung.com>

--- src/tdm_sprd_pp.c
+++ src/tdm_sprd_pp.c
@@ -125,15 +125,15 @@ void _tdm_sprd_pp_roadmap_print(tdm_sprd_pp_roadmap *roadmap)
 	TDM_DBG("Count of steps %d", roadmap->max_step);
 	for (i = 0; i < roadmap->max_step; i++) {
 		TDM_DBG("Step %d, Prop_id %d", i+1, roadmap->prop_id[i]);
-		TDM_DBG("format %c%c%c%c(%u) -> %c%c%c%c(%u)", roadmap->step_info[i].src_config.format,
+		TDM_DBG("format (%u)%c%c%c%c -> (%u)%c%c%c%c", roadmap->step_info[i].src_config.format,
 				SPRD_FOURCC_STR(roadmap->step_info[i].src_config.format), roadmap->step_info[i].dst_config.format,
 				SPRD_FOURCC_STR(roadmap->step_info[i].dst_config.format));
-		TDM_DBG("rotate+flip %u+%s", roadmap->step_info[i].transform % 4,
+		TDM_DBG("rotate+flip is %u+%s", roadmap->step_info[i].transform % 4,
 				roadmap->step_info[i].transform > 3?"Horizontal":"None");
-		TDM_DBG("size src->dst wxh (%u)x(%u) -> (%u)x(%u)", roadmap->step_info[i].src_config.size.h,
+		TDM_DBG("size src->dst (w)x(h) (%u)x(%u) -> (%u)x(%u)", roadmap->step_info[i].src_config.size.h,
 				roadmap->step_info[i].src_config.size.v, roadmap->step_info[i].dst_config.size.h,
 				roadmap->step_info[i].dst_config.size.v);
-		TDM_DBG("crop src->dst xy+w+h (%u)x(%u)+(%u)+(%u) -> (%u)x(%u)+(%u)+(%u)", roadmap->step_info[i].src_config.pos.x,
+		TDM_DBG("crop src->dst (x)(y)+(w)+(h) (%u)x(%u)+(%u)+(%u) -> (%u)x(%u)+(%u)+(%u)", roadmap->step_info[i].src_config.pos.x,
 				roadmap->step_info[i].src_config.pos.y, roadmap->step_info[i].src_config.pos.w,
 				roadmap->step_info[i].src_config.pos.h, roadmap->step_info[i].dst_config.pos.x,
 				roadmap->step_info[i].dst_config.pos.y, roadmap->step_info[i].dst_config.pos.w,
@@ -209,6 +209,7 @@ _tdm_sprd_pp_queue(tdm_sprd_pp_data *pp_data, unsigned int prop_id,
 	tdm_sprd_data *sprd_data = pp_data->sprd_data;
 	struct drm_sprd_ipp_queue_buf buf;
 	int i, bo_num, ret = 0;
+	uint32_t size= 0 , stride = 0, offset = 0;
 
 	CLEAR(buf);
 	buf.prop_id = prop_id;
@@ -220,6 +221,9 @@ _tdm_sprd_pp_queue(tdm_sprd_pp_data *pp_data, unsigned int prop_id,
 	for (i = 0; i < SPRD_DRM_PLANAR_MAX && i < bo_num; i++) {
 		tbm_bo bo = tbm_surface_internal_get_bo(src, i);
 		buf.handle[i] = (__u32)tbm_bo_get_handle(bo, TBM_DEVICE_DEFAULT).u32;
+		tbm_surface_internal_get_plane_data(src, i, &size, &offset, &stride);
+		TDM_DBG("pp %p(%d). prop_id(%d) src plane(%d) size(%u) offset(%u) stride(%u) ",
+				pp_data, pp_data->stamp, buf.prop_id, i, size, offset, stride);
 	}
 
 	TDM_DBG("pp %p(%d). prop_id(%d) ops_id(%d) ctrl(%d) id(%d) handles(%x %x %x). ",
@@ -245,6 +249,9 @@ _tdm_sprd_pp_queue(tdm_sprd_pp_data *pp_data, unsigned int prop_id,
 	for (i = 0; i < SPRD_DRM_PLANAR_MAX && i < bo_num; i++) {
 		tbm_bo bo = tbm_surface_internal_get_bo(dst, i);
 		buf.handle[i] = (__u32)tbm_bo_get_handle(bo, TBM_DEVICE_DEFAULT).u32;
+		tbm_surface_internal_get_plane_data(dst, i, &size, &offset, &stride);
+		TDM_DBG("pp %p(%d). prop_id(%d) dst plane(%d) size(%u) offset(%u) stride(%u) ",
+				pp_data, pp_data->stamp, buf.prop_id, i, size, offset, stride);
 	}
 
 	TDM_DBG("pp %p(%d). prop_id(%d) ops_id(%d) ctrl(%d) id(%d) handles(%x %x %x). ",
@@ -341,8 +348,76 @@ _tdm_sprd_pp_destroy_task (tdm_sprd_pp_data *pp_data, tdm_sprd_pp_task * task)
 		if (task->buffers[i].dst)
 			tbm_surface_internal_unref(task->buffers[i].dst);
 	}
+
+	if (task->done_func) {
+		TDM_DBG("pp %p(%d). Return src %p dst %p", pp_data, pp_data->stamp,
+				task->buffers[0].src, task->buffers[task->max_step-1].dst);
+		task->done_func(pp_data, task->buffers[0].src,
+						 task->buffers[task->max_step-1].dst,
+						 task->done_user_data);
+		}
 	TDM_DBG("pp %p(%d). Task %p(%d) released", pp_data, pp_data->stamp, task, task->stamp);
 	free(task);
+}
+
+static int
+_tdm_sprd_pp_make_new_tasks(tdm_sprd_pp_data *pp_data)
+{
+	tdm_sprd_pp_buffer *main_buffer = NULL, *b = NULL, *bb = NULL;
+	tdm_sprd_pp_task *new_task = NULL;
+	int i;
+	if (LIST_IS_EMPTY(&pp_data->pending_buffer_list)) {
+		TDM_WRN("pp %p(%d) buffer list is empty. Nothing to do", pp_data, pp_data->stamp);
+		return 0;
+	}
+	LIST_FOR_EACH_ENTRY_SAFE(b, bb, &pp_data->pending_buffer_list, link) {
+		main_buffer = b;
+		if ((new_task = calloc(1, sizeof(tdm_sprd_pp_task))) == NULL) {
+			TDM_ERR("Out of memory");
+			return -1;
+		}
+		LIST_DEL(&b->link);
+		memcpy(new_task->prop_id, pp_data->roadmap.prop_id, sizeof(unsigned int)*PP_MAX_STEP);
+		new_task->stamp = task_stamp++;
+		new_task->max_step = pp_data->roadmap.max_step;
+		new_task->current_step = 0;
+		new_task->done_func = pp_data->done_func;
+		new_task->done_user_data = pp_data->done_user_data;
+		tbm_surface_internal_ref(main_buffer->src);
+		new_task->buffers[0].src = main_buffer->src;
+		tbm_surface_internal_ref(main_buffer->dst);
+		new_task->buffers[new_task->max_step-1].dst = main_buffer->dst;
+
+#if 0
+		if (new_task->max_step > 1) {
+			tbm_surface_info_s src_buf_info;
+			tbm_surface_info_s dst_buf_info;
+			tbm_surface_get_info(main_buffer->src, &src_buf_info);
+			tbm_surface_get_info(main_buffer->dst ,&dst_buf_info);
+			unsigned int max_width = (src_buf_info.width > dst_buf_info.width) ? src_buf_info.width : dst_buf_info.width;
+			unsigned int max_height = (src_buf_info.height > dst_buf_info.height) ? src_buf_info.height : dst_buf_info.height;
+			tbm_format temp_buf_fmt = dst_buf_info.format;
+			for (i = 1; i < new_task->max_step; i++) {
+				new_task->buffers[i-1].dst = tbm_surface_create (max_width, max_height, temp_buf_fmt);
+				tbm_surface_internal_ref(new_task->buffers[i-1].dst);
+				new_task->buffers[i].src = new_task->buffers[i-1].dst;
+			}
+		}
+#endif
+		for (i = 1; i < new_task->max_step; i++) {
+			new_task->buffers[i-1].dst = tbm_surface_create (pp_data->roadmap.step_info[i-1].dst_config.size.h,
+															 pp_data->roadmap.step_info[i-1].dst_config.size.v,
+															 pp_data->roadmap.step_info[i-1].dst_config.format);
+			tbm_surface_internal_ref(new_task->buffers[i-1].dst);
+			new_task->buffers[i].src = new_task->buffers[i-1].dst;
+			}
+		LIST_ADDTAIL(&new_task->link, &pp_data->pending_tasks_list);
+		TDM_DBG("pp %p(%d). Add new src %p dst %p buffer", pp_data, pp_data->stamp, main_buffer->src, main_buffer->dst);
+		TDM_DBG("To New task %p(%d)", new_task, new_task->stamp);
+		free(main_buffer);
+		main_buffer = NULL;
+	}
+	return 1;
 }
 
 static tdm_error
@@ -371,6 +446,7 @@ _tdm_sprd_pp_worker (tdm_sprd_pp_data *pp_data)
 				return TDM_ERROR_NONE;
 			}
 			pp_data->current_task_p = NULL;
+/*
 			if (done_task->done_func) {
 					TDM_DBG("pp %p(%d). Return src %p dst %p", pp_data, pp_data->stamp,
 							done_task->buffers[0].src, done_task->buffers[done_task->max_step-1].dst);
@@ -381,13 +457,41 @@ _tdm_sprd_pp_worker (tdm_sprd_pp_data *pp_data)
 			else {
 				TDM_WRN("pp %p(%d). No done func", pp_data, pp_data->stamp);
 			}
+*/
 			_tdm_sprd_pp_destroy_task(pp_data, done_task);
 		}
 		else {
-			TDM_WRN("pp %p(%d). Busy", pp_data, pp_data->stamp);
+			TDM_INFO("pp %p(%d). Still converting, add task to queue", pp_data, pp_data->stamp);
 			return TDM_ERROR_NONE;
 		}
 	}
+
+	if (pp_data->roadmap_changed == 1) {
+		tdm_sprd_pp_task *temp_task = NULL, *temp_task_next = NULL;
+		int i = 0;
+		LIST_FOR_EACH_ENTRY_SAFE(temp_task, temp_task_next, &pp_data->pending_tasks_list, link) {
+			LIST_DEL(&temp_task->link);
+			_tdm_sprd_pp_destroy_task(pp_data, temp_task);
+		}
+		_tdm_sprd_pp_roadmap_copy(&pp_data->roadmap, &pp_data->new_roadmap);
+		for (i = 0; i < pp_data->roadmap.max_step; i++) {
+			pp_data->roadmap.prop_id[i] = _tdm_sprd_pp_set(pp_data, &pp_data->roadmap.step_info[i], pp_data->roadmap.prop_id[i]);
+			if (pp_data->roadmap.prop_id[i] <= 0) {
+				TDM_ERR("pp %p(%d). Can't setup converter", pp_data, pp_data->stamp);
+				return TDM_ERROR_BAD_REQUEST;
+			}
+		}
+		pp_data->roadmap_changed = 0;
+		_tdm_sprd_pp_roadmap_print(&pp_data->roadmap);
+		if (pp_data->new_buffers == 1) {
+			if (_tdm_sprd_pp_make_new_tasks(pp_data) < 0) {
+				TDM_ERR("pp %p(%d). Can't create new task", pp_data, pp_data->stamp);
+				return TDM_ERROR_BAD_REQUEST;
+			}
+			pp_data->new_buffers = 0;
+		}
+	}
+
 	if (!LIST_IS_EMPTY(&pp_data->pending_tasks_list)) {
 		next_task = (tdm_sprd_pp_task * )container_of(pp_data->pending_tasks_list.next, next_task, link);
 		LIST_DEL(&next_task->link);
@@ -409,12 +513,14 @@ _tdm_sprd_pp_worker (tdm_sprd_pp_data *pp_data)
 		}
 		next_task->status = TASK_CONVERTING;
 		pp_data->current_task_p = next_task;
+		return TDM_ERROR_NONE;
 	}
 	else {
 		TDM_DBG("pp %p(%d). Nothing to do", pp_data, pp_data->stamp);
 	}
 	return TDM_ERROR_NONE;
 }
+
 
 void
 tdm_sprd_pp_handler(struct drm_sprd_ipp_event *hw_ipp_p)
@@ -651,6 +757,8 @@ _sprd_pp_make_roadmap(tdm_sprd_pp_data *pp_data, tdm_info_pp *info)
 		pp_data->new_roadmap.step_info[i].flags = pp_data->new_roadmap.step_info[i - 1].flags;
 	}
 	memcpy(&pp_data->new_roadmap.step_info[max_size - 1].dst_config, &info->dst_config, sizeof(tdm_info_config));
+	pp_data->new_roadmap.step_info[max_size-1].dst_config.pos.h = height_leap[height_leap_size - 1];
+	pp_data->new_roadmap.step_info[max_size-1].dst_config.pos.w = width_leap[width_leap_size - 1];
 	pp_data->new_roadmap.max_step = max_size;
 	//_sprd_pp_make_roadmap_transform (pp_data, info);
 	return TDM_ERROR_NONE;
@@ -698,65 +806,7 @@ sprd_pp_attach(tdm_pp *pp, tbm_surface_h src, tbm_surface_h dst)
 	return TDM_ERROR_NONE;
 }
 
-static int
-_tdm_sprd_pp_make_new_tasks(tdm_sprd_pp_data *pp_data)
-{
-	tdm_sprd_pp_buffer *main_buffer = NULL, *b = NULL, *bb = NULL;
-	tdm_sprd_pp_task *new_task = NULL;
-	int i;
-	if (LIST_IS_EMPTY(&pp_data->pending_buffer_list)) {
-		TDM_WRN("pp %p(%d) buffer list is empty. Nothing to do", pp_data, pp_data->stamp);
-		return 0;
-	}
-	LIST_FOR_EACH_ENTRY_SAFE(b, bb, &pp_data->pending_buffer_list, link) {
-		main_buffer = b;
-		if ((new_task = calloc(1, sizeof(tdm_sprd_pp_task))) == NULL) {
-			TDM_ERR("Out of memory");
-			return -1;
-		}
-		LIST_DEL(&b->link);
-		memcpy(new_task->prop_id, pp_data->roadmap.prop_id, sizeof(unsigned int)*PP_MAX_STEP);
-		new_task->stamp = task_stamp++;
-		new_task->max_step = pp_data->roadmap.max_step;
-		new_task->current_step = 0;
-		new_task->done_func = pp_data->done_func;
-		new_task->done_user_data = pp_data->done_user_data;
-		tbm_surface_internal_ref(main_buffer->src);
-		new_task->buffers[0].src = main_buffer->src;
-		tbm_surface_internal_ref(main_buffer->dst);
-		new_task->buffers[new_task->max_step-1].dst = main_buffer->dst;
 
-#if 0
-		if (new_task->max_step > 1) {
-			tbm_surface_info_s src_buf_info;
-			tbm_surface_info_s dst_buf_info;
-			tbm_surface_get_info(main_buffer->src, &src_buf_info);
-			tbm_surface_get_info(main_buffer->dst ,&dst_buf_info);
-			unsigned int max_width = (src_buf_info.width > dst_buf_info.width) ? src_buf_info.width : dst_buf_info.width;
-			unsigned int max_height = (src_buf_info.height > dst_buf_info.height) ? src_buf_info.height : dst_buf_info.height;
-			tbm_format temp_buf_fmt = dst_buf_info.format;
-			for (i = 1; i < new_task->max_step; i++) {
-				new_task->buffers[i-1].dst = tbm_surface_create (max_width, max_height, temp_buf_fmt);
-				tbm_surface_internal_ref(new_task->buffers[i-1].dst);
-				new_task->buffers[i].src = new_task->buffers[i-1].dst;
-			}
-		}
-#endif
-		for (i = 1; i < new_task->max_step; i++) {
-			new_task->buffers[i-1].dst = tbm_surface_create (pp_data->roadmap.step_info[i-1].dst_config.size.h,
-															 pp_data->roadmap.step_info[i-1].dst_config.size.v,
-															 pp_data->roadmap.step_info[i-1].dst_config.format);
-			tbm_surface_internal_ref(new_task->buffers[i-1].dst);
-			new_task->buffers[i].src = new_task->buffers[i-1].dst;
-			}
-		LIST_ADDTAIL(&new_task->link, &pp_data->pending_tasks_list);
-		TDM_DBG("pp %p(%d). Add new src %p dst %p buffer", pp_data, pp_data->stamp, main_buffer->src, main_buffer->dst);
-		TDM_DBG("To New task %p(%d)", new_task, new_task->stamp);
-		free(main_buffer);
-		main_buffer = NULL;
-	}
-	return 1;
-}
 
 tdm_error
 sprd_pp_commit(tdm_pp *pp)
@@ -764,23 +814,31 @@ sprd_pp_commit(tdm_pp *pp)
 	tdm_sprd_pp_data *pp_data = (tdm_sprd_pp_data *) pp;
 	RETURN_VAL_IF_FAIL(_tdm_sprd_pp_check_struct(pp_data), TDM_ERROR_INVALID_PARAMETER);
 	if (pp_data->roadmap_changed) {
-		unsigned int i;
+		unsigned int i, need_postpond = 0;
 		for (i = 0; i < pp_data->roadmap.max_step; i++) {
-			if (pp_data->roadmap.prop_id[i] > 0)
-				_tdm_sprd_pp_cmd(pp_data, pp_data->roadmap.prop_id[i], IPP_PAUSE);
-		}
-		_tdm_sprd_pp_roadmap_copy(&pp_data->roadmap, &pp_data->new_roadmap);
-		for (i = 0; i < pp_data->roadmap.max_step; i++) {
-			pp_data->roadmap.prop_id[i] = _tdm_sprd_pp_set(pp_data, &pp_data->roadmap.step_info[i], pp_data->roadmap.prop_id[i]);
-			if (pp_data->roadmap.prop_id[i] <= 0) {
-				TDM_ERR("pp %p(%d). Can't setup converter", pp_data, pp_data->stamp);
-				return TDM_ERROR_BAD_REQUEST;
+			if (pp_data->roadmap.prop_id[i] > 0) {
+				tdm_sprd_prop_id * found_prop = _find_prop_id(pp_data, pp_data->roadmap.prop_id[i]);
+				if (found_prop != NULL &&
+						(found_prop->status == IPP_CTRL_PLAY || found_prop->status == IPP_CTRL_RESUME)) {
+//					_tdm_sprd_pp_cmd(pp_data, pp_data->roadmap.prop_id[i], IPP_PAUSE);
+					need_postpond = 1;
+				}
 			}
 		}
-		pp_data->roadmap_changed = 0;
-		_tdm_sprd_pp_roadmap_print(&pp_data->roadmap);
+		if (!need_postpond) {
+			_tdm_sprd_pp_roadmap_copy(&pp_data->roadmap, &pp_data->new_roadmap);
+			for (i = 0; i < pp_data->roadmap.max_step; i++) {
+				pp_data->roadmap.prop_id[i] = _tdm_sprd_pp_set(pp_data, &pp_data->roadmap.step_info[i], pp_data->roadmap.prop_id[i]);
+				if (pp_data->roadmap.prop_id[i] <= 0) {
+					TDM_ERR("pp %p(%d). Can't setup converter", pp_data, pp_data->stamp);
+					return TDM_ERROR_BAD_REQUEST;
+				}
+			}
+			pp_data->roadmap_changed = 0;
+			_tdm_sprd_pp_roadmap_print(&pp_data->roadmap);
+		}
 	}
-	if (pp_data->new_buffers == 1) {
+	if (pp_data->new_buffers == 1 && pp_data->roadmap_changed == 0) {
 		if (_tdm_sprd_pp_make_new_tasks(pp_data) < 0) {
 			TDM_ERR("pp %p(%d). Can't create new task", pp_data, pp_data->stamp);
 			return TDM_ERROR_BAD_REQUEST;
